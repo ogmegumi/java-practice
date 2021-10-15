7d306b82380af443d958f6c6812c138b4caa1b68
GitHubのユーザ名 <ogmegumi@gmail.com>
2021-10-15 23:18:24 +0900

    add:variable02

---
+++ src/Variable02.java
@@ -0,0 +1,11 @@
+
+public class Variable02 {
+	public static void main(String[] args) {
+	        final int LUCKY_NUMBER = 12;
+	        String str = "太田恵";
+	        System.out.println(str);
+	        str = "少年よ大志を抱け";
+	        System.out.println(str);
+	        System.out.println(LUCKY_NUMBER);
+	    }
+	}

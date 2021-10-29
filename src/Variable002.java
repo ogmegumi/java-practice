ed146ad3bf13d7f8a6ef7ad7fa45c3c02b1a7f41
GitHubのユーザ名 <ogmegumi@gmail.com>
2021-10-29 11:00:51 +0900

    add:learned hennsuu

---
+++ src/Variable002.java
@@ -0,0 +1,15 @@
+
+public class Variable002 {
+	public static void main(String[] args) {
+		String name1;
+		name1 = "megumi";
+		System.out.println(name1);
+
+		name1 = "megu";
+		System.out.println(name1);
+
+		int namae1;
+		namae1 = 12;
+		System.out.println(namae1);
+	}
+}

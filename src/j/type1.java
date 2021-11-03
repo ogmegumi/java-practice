1925f5fcdb4fb118f973c36f99b7b267dadeb372
GitHubのユーザ名 <ogmegumi@gmail.com>
2021-11-03 21:54:53 +0900

    add:chapter05

--- src/j/type1.java
+++ src/j/type1.java
@@ -12,5 +12,7 @@ public class type1 {
 	        // nullの変数を参照するとNullPointerExceptionのエラー
 	        string1 = null;
 	        System.out.println(string1 + "の文字数：" + string1.length());
+	        
+	        
 	}
 }
